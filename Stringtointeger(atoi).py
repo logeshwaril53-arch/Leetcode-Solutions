146c142ffc8edbd38c23dc075c1c59ea9fe915b4
logeshwaril53-arch <logeshwaril53@gmail.com>
2026-07-10 21:04:23 +0530

    Add myAtoi function to convert string to integer
    Implement myAtoi function to convert string to integer with handling for leading spaces, signs, and overflow.

---
+++ Stringtointeger(atoi).py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.lstrip()  # Remove leading spaces
+
+        if not s:
+            return 0
+
+        sign = 1
+        i = 0
+
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        result = 0
+
+        while i < len(s) and s[i].isdigit():
+            result = result * 10 + int(s[i])
+            i += 1
+
+        result *= sign
+
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+
+        return result
